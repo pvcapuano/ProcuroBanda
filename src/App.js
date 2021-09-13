c9eb3debc687247afea7080e9de932206fe2f7e2
pvcapuano <pvcapuano@gmail.com>
2021-09-13 16:30:39 -0300

    inserindo routers

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import NavBar from './Components/NavBar';
 import ProcuroBanda from './Components/ProcuroBanda';
 import AllUsers from './Components/AllUsers';
 import AddUser from './Components/AddUser';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <NavBar />
-    <ProcuroBanda />
-    <AllUsers />
-    <AddUser />
-    
-    </>
+    <Switch>
+      <Route path="/" exact component={ProcuroBanda} />
+      <Route path="/all" exact component={AllUsers} />
+      <Route path="/add" exact component={AddUser} />
+      </Switch>  
+    </BrowserRouter>
   );
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
+import { NavLink } from 'react-router-dom'
+
 
 const useStyle = makeStyles({
     header: {
@@ -11,9 +13,9 @@ export default function NavBar() {
     return(
         <AppBar className={classes.header} position="static">
             <Toolbar>
-                <Typography component="h2">Procuro Banda</Typography>
-                <Typography component="h2">Todos os músicos</Typography>
-                <Typography component="h2">Adicionar Músico</Typography>
+                <NavLink to="./">Procuro Banda</NavLink>
+                <NavLink to="all">Todos os músicos</NavLink>
+                <NavLink to="add">Adicionar Músico</NavLink>
             </Toolbar>
         </AppBar>
     )
