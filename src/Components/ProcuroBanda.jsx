c6ed10648870e67f385e515556b4ffa87974ea03
pvcapuano <pvcapuano@gmail.com>
2021-09-13 17:50:54 -0300

    pagina principal

--- src/Components/ProcuroBanda.jsx
+++ src/Components/ProcuroBanda.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import GuitarJump from '../Assets/Images/guitar_jump.png'
+import { Box, makeStyles, Typography } from '@material-ui/core'
+
+const useStyle = makeStyles({
+    component: {
+        margin: '200px 0 0 40%',
+        width: '50%'
+    }
+})
 
 export default function ProcuroBanda() {
+    const classes = useStyle()
     return (
-        <>
-        
-        <p>Bandas kd vcs?</p>
-        </>
+        <Box className={classes.component} >
+            <Typography variant="h4" style={{marginBottom: 50}}>Procuro Banda</Typography>
+            <Box style={{display: 'flex'}}>
+                <img src={GuitarJump} alt="" />
+            </Box>
+        </Box>
     )
 }
