eba1dfe3ba09510e4d7508b22cd7b9d560d10128
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-10-25 09:07:30 -0300

    config mongodb

--- server/index.js
+++ server/index.js
@@ -3,31 +3,19 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
+
 const Post = require("./models/Post");
 const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
   type Query {
-    getPosts: [Post]
+    sayHi: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
+    sayHi: () => "hello my friend",
   },
 };
 
