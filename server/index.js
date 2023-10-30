45cd7d00ab4a065b18057a1847c1431207a9df0a
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-10-30 19:31:21 -0300

    config mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,21 +1,32 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
-
 require("dotenv").config();
 
 const Post = require("./models/Post");
-const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "hello my friend",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
@@ -24,12 +35,15 @@ const server = new ApolloServer({
   resolvers,
 });
 
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log("MongoDB Connected");
-    return server.listen({ port: 5000 });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  });
+const connectDB = require("../server/config/db");
+
+connectDB().then(() => {
+  server
+    .listen({ port: 5000 })
+    .then((res) => {
+      console.log(`Server running at ${res.url}`);
+    })
+    .catch((error) => {
+      console.error(`Error starting Apollo server: ${error}`);
+    });
+});
