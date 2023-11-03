32e9d3d1a648ac85ffd999866287847e6f7fc612
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-11-03 18:05:38 -0300

    add login validation

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -2,12 +2,52 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
-const { validateRegisterInput } = require("../../util/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
 require("dotenv").config();
 const User = require("../../models/User");
 
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.SECRETE_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("Wrong credential", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return { ...user._doc, id: user._id, token };
+    },
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
@@ -45,15 +85,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        process.env.SECRETE_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
       return { ...res._doc, id: res._id, token };
     },
   },
